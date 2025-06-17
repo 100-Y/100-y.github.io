b33265cf51bb705af74393b08676e809ced56bd8
100-Y <1@36525.day>
2025-06-17 14:26:22 +0000

    Deploying to gh-pages from @ 100-Y/100-y.github.io@ca465d4acea85049ac8aed2779480ebc2d3383d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4547,4 +4547,79 @@ ninja.data = [{
             window.location.href = "/blog/2010-09-15/ordinary-stars/";
           
         },
-      },];
+      },{
+        id: 'social-youtube',
+        title: 'YouTube',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://youtube.com/@36525_day", "_blank");
+        },
+      },{
+        id: 'social-pinterest',
+        title: 'Pinterest',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.pinterest.com/36525day", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/100-Y/100-y.github.io", "_blank");
+        },
+      },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:", "_blank");
+        },
+      },{
+        id: 'social-medium',
+        title: 'Medium',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://medium.com/@36525.day", "_blank");
+        },
+      },{
+        id: 'social-wechat_qr',
+        title: 'Wechat_qr',
+        section: 'Socials',
+        handler: () => {
+          window.open("", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/36525_day", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
